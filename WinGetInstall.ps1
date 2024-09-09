f213ff0dfcb3fb5e98968b5cbb25e6af586a5b0b
eclipse313 <tvdw@server-start.de>
2024-09-09 15:31:02 +0200

    Struktur und Dateinamen angepasst

--- WinGetInstall.ps1
+++
@@ -1,5 +0,0 @@
-# Neuste Version von WinGet installieren
-Add-AppxPackage -Path "https://aka.ms/getwinget"
-
-# Serverstart Source zu Winget hinzufügen
-winget source add -n serverstart -a https://winget.server-start.net/91fcba9f-58ae-475c-8511-a6f4872871ff -t "Microsoft.Rest"
