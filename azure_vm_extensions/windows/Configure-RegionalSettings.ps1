0a1c78512ada6b015add4577c17b80bb78be4289
eclipse313 <tvdw@server-start.de>
2024-10-15 21:41:44 +0200

    wip: Test

--- azure_vm_extensions/windows/Configure-RegionalSettings.ps1
+++ azure_vm_extensions/windows/Configure-RegionalSettings.ps1
@@ -35,20 +35,117 @@ try {
 # Load the .DEFAULT registry hive
 reg load HKU\.DEFAULT "C:\Users\Default\NTUSER.DAT"
 
-# Set date and time format for the default profile
-$registryPath = "HKU\.DEFAULT\Control Panel\International"
+$LocaleSettings = @"
+Windows Registry Editor Version 5.00
 
-if (Test-Path $registryPath) {
-    Set-ItemProperty -Path $registryPath -Name "sShortDate" -Value "dd.MM.yyyy"
-    Set-ItemProperty -Path $registryPath -Name "sLongDate" -Value "dddd, dd. MMMM yyyy"
-    Set-ItemProperty -Path $registryPath -Name "sTimeFormat" -Value "HH:mm:ss"
-    Write-Host "Date and time formats for default user profile have been updated."
-} else {
-    Write-Host "Registry path for default user profile not found."
-}
-
-# Unload the .DEFAULT registry hive after making changes
-reg unload HKU\.DEFAULT
+[HKEY_CURRENT_USER\Control Panel\International]
+"Locale"="00000407"
+"sShortDate"="dd.MM.yyyy"
+"sDate"="."
+"sLongDate"="dddd, d. MMMM yyyy"
+"sTime"="HH:mm:ss"
+"s1159"=""
+"s2359"=""
+"sTimeFormat"="HH:mm:ss"
+"iCountry"="49"
+"iDate"="1"
+"iTime"="1"
+"iTLZero"="1"
+"iCurrency"="3"
+"iNegCurr"="1"
+"sCurrency"="€"
+"sMonDecimalSep"=","
+"sMonThousandSep"="."
+"iDigits"="2"
+"iLZero"="1"
+"sDecimal"=","
+"sThousand"="."
+"iMeasure"="0"
+"iPaperSize"="9"
+"iDefaultUILanguage"="1031"
+"iLangID"="1031"
+"LocaleName"="de-DE"
+"@
+  
+# Default Profile:
+Write-Verbose ('Writing registry values for profile: DEFAULT') -Verbose
+Write-RegistryWithHiveLoad -RegFileContents $LocaleSettings -DatFilePath C:\Users\Default\NTUSER.DAT
 
 # Log message for completion
 Write-Host "Alle regionalen Einstellungen und Eingabemethoden wurden angewendet." -ForegroundColor Green
+
+
+
+
+
+
+
+
+
+function Get-TempRegFilePath {
+    (Join-Path -Path ([IO.Path]::GetTempPath()) -ChildPath ([guid]::NewGuid().Guid)) + '.reg'
+}
+
+function Write-Registry {
+    param($RegFileContents, $UserSid)
+    
+    $TempRegFile = Get-TempRegFilePath
+    $regFileContents = $regFileContents -replace 'HKEY_CURRENT_USER', "HKEY_USERS\$userSid"
+    $regFileContents | Out-File -FilePath $TempRegFile
+    
+    $p = Start-Process -FilePath C:\Windows\regedit.exe -ArgumentList @('/s', $TempRegFile) -PassThru
+    do { Start-Sleep -Seconds 1 } while (-not $p.HasExited)
+    
+    Remove-Item -Path $TempRegFile -Force
+}
+
+function Write-RegistryWithHiveLoad {
+    param($RegFileContents, $DatFilePath)
+    
+    $hiveName = 'x_' +  ($user = (($datFilePath -split '\\')[-2]).ToUpper())
+
+    try {
+        if(-not (IsFileLocked -Path $DatFilePath)) {
+            $null = C:\Windows\System32\reg.exe load "HKU\$hiveName" $DatFilePath
+            if($LASTEXITCODE -ne 0) { throw 'Error loading the DAT file' }
+    
+            $TempRegFile = Get-TempRegFilePath
+            $regFileContents = $regFileContents -replace 'HKEY_CURRENT_USER', "HKEY_USERS\$hiveName"
+            $regFileContents | Out-File -FilePath $TempRegFile
+
+            $p = Start-Process -FilePath C:\Windows\regedit.exe -ArgumentList @('/s', $TempRegFile) -PassThru
+            do { Start-Sleep -Seconds 1 } while (-not $p.HasExited)
+
+            $null = C:\Windows\System32\reg.exe unload "HKU\$hiveName"
+
+            Remove-Item -Path $TempRegFile -Force
+        } else {
+            Write-Verbose ('Skipped user {0}. File {1} is locked by another process' -f $user, $DatFilePath) -Verbose
+        }
+    } catch {
+        Write-Verbose $_.Exception.Message -Verbose
+    }
+}
+
+function IsFileLocked {
+    param([string]$Path)
+
+    [bool] $isFileLocked = $true
+    $file = $null
+
+    try {
+        $file = [IO.File]::Open(
+            $Path, [IO.FileMode]::Open, [IO.FileAccess]::Read, [IO.FileShare]::None
+        )
+        $isFileLocked = $false
+    } catch [IO.IOException] {
+        if ($_.Exception.Message -notmatch 'used by another process') {
+            throw $_.Exception
+        }
+    } finally {
+        if ($null -ne $file) {
+            $file.Close()
+        }
+    }
+    $isFileLocked
+}
