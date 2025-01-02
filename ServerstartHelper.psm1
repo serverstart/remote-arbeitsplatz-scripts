19380c6a0bbdf9a1719c20ae06dcc05df11e2f9d
eclipse313 <tvdw@server-start.de>
2025-01-02 19:04:59 +0100

    wip: WIP

--- ServerstartHelper.psm1
+++ ServerstartHelper.psm1
@@ -1,21 +1,21 @@
 $private:ServerstartPath = $null
 
-function Get-ServerStartDirectory {
-   <# 
-   .SYNOPSIS
-   Ermittelt und erstellt bei Bedarf das Serverstart-Verzeichnis.
-   Versucht erst %ProgramFiles%\serverstart, dann %AppData%\serverstart.
-   Ergebnis wird für weitere Aufrufe gecached.
-   #>
-   if (-not $private:ServerstartPath) {
-       $paths = @(
-           "$env:ProgramFiles\serverstart",
-           "$env:APPDATA\serverstart"
-       )
-       $private:ServerstartPath = $paths | Select-Object -First 1 {
-           Test-Path $_ -ErrorAction SilentlyContinue -or 
-           (New-Item -Path $_ -ItemType Directory -Force -ErrorAction SilentlyContinue)
-       }
-   }
-   return $private:ServerstartPath
+function Get-ServerstartDirectory {
+    <# 
+    .SYNOPSIS
+    Ermittelt und erstellt bei Bedarf das Serverstart-Verzeichnis.
+    Versucht erst %ProgramFiles%\serverstart, dann %AppData%\serverstart.
+    Ergebnis wird für weitere Aufrufe gecached.
+    #>
+    if (-not $private:ServerstartPath) {
+        $paths = @(
+            "$env:ProgramFiles\serverstart",
+            "$env:APPDATA\serverstart"
+        )
+        $private:ServerstartPath = $paths | Where-Object { 
+            Test-Path $_ -ErrorAction SilentlyContinue -or 
+            (New-Item -Path $_ -ItemType Directory -Force -ErrorAction SilentlyContinue) 
+        } | Select-Object -First 1
+    }
+    return $private:ServerstartPath
 }
